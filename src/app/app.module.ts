60eab5301b29cb60e510a716afd47ffeec1eb1c4
Victor Zuñiga <victorhugo.zuniga@outlook.com>
2023-07-24 22:43:04 -0600

    Proyecto de super heroes en angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,8 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
