02241d34d26efd406483b8cef38c6468fd25d4ba
Victor Zuñiga <victorhugo.zuniga@outlook.com>
2023-07-25 22:16:31 -0600

    Html nuevo, editar y eliminar heroe, buscador de heroes

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-hero-page',
@@ -6,6 +10,29 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
+  public hero?: Hero;
 
+  constructor(
+    private activatedRouter: ActivatedRoute,
+    private heroService: HeroService,
+    private router: Router
+  ) { }
+
+  goBack(): void {
+    this.router.navigateByUrl('heroes/list');
+  }
+
+  ngOnInit(): void {
+    this.activatedRouter.params
+      .pipe(
+        switchMap(({ id }) => this.heroService.getHeroById(id))
+      ).subscribe(hero => {
+        if (!hero) return this.router.navigate(['/heroes/list']);
+
+        this.hero = hero;
+
+        return;
+      });
+  }
 }
--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroService } from '../../services/heroes.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
@@ -7,5 +11,27 @@ import { Component } from '@angular/core';
   ]
 })
 export class SearchPageComponent {
+  public searchInput = new FormControl('');
+  public heroes: Hero[] = [];
+  public selectedHeroe?: Hero;
 
+  constructor(private heroService: HeroService) { }
+
+  searchHero() {
+    const value = this.searchInput.value || '';
+
+    this.heroService.getSuggestions(value)
+      .subscribe(
+        heroes => this.heroes = heroes
+      )
+  }
+
+  onSelectedOption(event: MatAutocompleteSelectedEvent): void {
+    if (!event.option.value)
+      this.selectedHeroe = undefined;
+
+    const hero: Hero = event.option.value;
+    this.searchInput.setValue(hero.superhero);
+    this.selectedHeroe = hero;
+  }
 }
